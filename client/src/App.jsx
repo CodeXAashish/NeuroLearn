4fae750159e016bf1267201ee99423a8cde283c4
CodeXAashish <aashishchauhan5935@gmail.com>
2026-05-15 19:16:40 +0530

    home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
 
 function App() {
   return (
-    <div className="h-screen bg-black text-white flex items-center justify-center text-4xl">
-      NeuroLearn
+    <div className="h-screen bg-black text-white flex flex-col items-center justify-center ">
+      <h1 className="text-5xl font-bold">
+            NeuroLearn
+      </h1>
+
+      <h3 className="text-2xl mt-4">
+             AI Powered Adaptive Learning System
+      </h3>
+      
     </div>
   )
 }
