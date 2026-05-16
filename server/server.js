ea6eddda3b8004b15eb9e6bb5cd9da621b6422a1
CodeXAashish <aashishchauhan5935@gmail.com>
2026-05-16 17:44:24 +0530

    Build  and check API

--- server/server.js
+++ server/server.js
@@ -4,14 +4,23 @@ require("dotenv").config()
 
 const connectDB = require("./config/db")
 
+const authRoutes = require("./routes/authRoutes")
+
 const app = express()
 connectDB()
 
 app.use(cors())
 app.use(express.json())
 
+app.use("/api/auth", authRoutes)
+
 app.get("/", (req, res) => {
   res.send("NeuroLearn API Running...")
+})
+app.get("/api/test", (req, res) => {
+  res.json({
+    message: "NeuroLearn API working..."
+ })
 })
 
 const PORT = process.env.PORT || 5000
@@ -19,3 +28,4 @@ const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
+
