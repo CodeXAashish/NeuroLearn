4e8ecb4df7ceb49af7ce3bca7f9426a25624daab
CodeXAashish <aashishchauhan5935@gmail.com>
2026-07-13 12:26:55 +0530

    build flashcard service

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,7 @@ import Quiz from "./pages/Quiz"
 import Chat from "./pages/Chat"
 import Planner from "./pages/Planner"
 import Notes from "./pages/Notes"
+import Flashcards from "./pages/Flashcards"
 
 function App() {
   return (
@@ -26,13 +27,13 @@ function App() {
 
          <Route path="/quiz" element={<Quiz />} />
 
-        <Route path="/chat" element={<Chat />} />
+         <Route path="/chat" element={<Chat />} />
 
          <Route path="/planner" element={<Planner />} />
 
          <Route path = "/notes" element = {<Notes/>}/>
 
-        
+         <Route path="/flashcards" element = { <Flashcards /> }/>
 
     </Routes>
   )
